ec828a347721a0fac41378008383a21e493c5416
Yahor Sinkevich2 <yahor.si@gmail.com>
2019-11-27 12:59:50 +0300

    added local List version with /0version dropped

--- LoopOverArrayOfBytes.cs
+++ LoopOverArrayOfBytes.cs
@@ -1,10 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System.Collections.Generic;
+using ListNew = System.Collections.GenericNew.List<byte>;
 
 namespace ListForVsForeach
 {
     public class LoopOverArrayOfBytes
     {
+        private ListNew _byteDataListNew;
         private List<byte> _byteDataList;
 
         [Params(16, 256, 1024)]
@@ -22,12 +24,28 @@ namespace ListForVsForeach
 
             _byteDataList = new List<byte>(byteData.Length);
             _byteDataList.AddRange(byteData);
+
+            _byteDataListNew = new ListNew(byteData.Length);
+            _byteDataListNew.AddRange(byteData);
         }
 
         [Benchmark(Baseline = true)]
         public int For()
         {
             var data = _byteDataList;
+            int sum = 0;
+
+            for (int i = 0; i < data.Count; ++i)
+            {
+                sum += data[i];
+            }
+
+            return sum;
+        }
+
+        public int ForNew()
+        {
+            var data = _byteDataListNew;
             int sum = 0;
 
             for (int i = 0; i < data.Count; ++i)
@@ -52,5 +70,18 @@ namespace ListForVsForeach
             return sum;
         }
 
+        [Benchmark]
+        public int ForeachNew()
+        {
+            var data = _byteDataListNew;
+            int sum = 0;
+
+            foreach (var n in data)
+            {
+                sum += n;
+            }
+
+            return sum;
+        }
     }
 }
