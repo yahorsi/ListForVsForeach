c2af2493b8fe254e2f9ba18da773652f5b92b557
Yahor Sinkevich2 <yahor.si@gmail.com>
2019-11-27 11:16:22 +0300

    initial

---
+++ LoopOverArrayOfBytes.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace ListForVsForeach
+{
+    public class LoopOverArrayOfBytes
+    {
+        private List<byte> _byteDataList;
+
+        [Params(16, 256, 1024)]
+        public int N { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var byteData = new byte[N];
+
+            for (int i = 0; i < byteData.Length; ++i)
+            {
+                byteData[i] = (byte)i;
+            }
+
+            _byteDataList = new List<byte>(byteData.Length);
+            _byteDataList.AddRange(byteData);
+        }
+
+        [Benchmark(Baseline = true)]
+        public int For()
+        {
+            var data = _byteDataList;
+            int sum = 0;
+
+            for (int i = 0; i < data.Count; ++i)
+            {
+                sum += data[i];
+            }
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int Foreach()
+        {
+            var data = _byteDataList;
+            int sum = 0;
+
+            foreach (var n in data)
+            {
+                sum += n;
+            }
+
+            return sum;
+        }
+
+    }
+}
